a8cb8b5caa143659eb2952ee3de40af499689532
lakhan368 <lakhan368@outlook.com>
2023-09-30 19:53:30 +0530

    s

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -11,7 +11,7 @@ function ProductScreen() {
     const product = products.find((p) => p._id == id)
     return (
     <div>
-        <Link to="/" className='btn btn-light my-3'> Go Back</Link>    
+        <Link to="/" className='btn btn-light my-3'> Go..Back</Link>    
         <Row>
          <Col md={6}>
           <Image src={product.image} alt={product.name} fluid/>
