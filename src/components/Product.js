729f71bb7716741a0de53567d96d9d4797a4cf4e
lakhan368 <lakhan368@outlook.com>
2023-09-30 23:43:50 +0530

    adding data

--- src/components/Product.js
+++ src/components/Product.js
@@ -24,7 +24,7 @@ function Product({ product }) {
         </Card.Text>
 
         <Card.Text as="h3">
-          Rs {product.price}
+        Rs {product.price}
         </Card.Text>
       </Card.Body>
     </Card>
