21611cc7e0666b197ec9fa81724cf3b65e27e16e
lakhan368 <lakhan368@outlook.com>
2023-09-30 19:09:18 +0530

    y

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Container, Row, Col} from 'react-bootstrap'
+
+function Footer() {
+    return (
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center'>Copyright &copy; lark-system 2023</Col>
+                    </Row>
+            </Container>
+
+        </footer>
+    )
+}
+
+export default Footer
